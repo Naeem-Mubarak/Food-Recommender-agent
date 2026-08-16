73552c83da7d0c3287682147b505c62f72b76a96
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-16 23:34:24 +0500

    Getting Strucutrd output from model

--- English_translator.py
+++ English_translator.py
@@ -1,20 +1,38 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import StrOutputParser
 from config import translator_llm_prompt
 from voice_llm import voice_transcript_generator,path
+from pydantic import BaseModel,Field
+from typing import Annotated,Optional,Dict
+from config import model
 
-data=voice_transcript_generator(path)
+
+
+
+class order_info(BaseModel):
+
+    taste : Annotated[Optional[str],Field(description='taste of food customer orders spicy or sweet')]
+    budget : Annotated[Optional[str],Field(description='Budget of the user')]
+
+
+class order_schema(BaseModel):
+
+    id : Annotated[int,Field(ge=1,description='Unique id of the customer')]
+    name : Annotated[str,Field(description='Name of the customer')]
+    order : Annotated[str,Field(description='Order customer interested in')]
+    order_info : order_info
+
+
+
+
 
 def english_translator(data):
 
-    parser=StrOutputParser()
-    model=ChatGoogleGenerativeAI(model='gemini-3.5-flash')
-    chain = translator_llm_prompt | model | parser
+    
+    structured_llm=model.with_structured_output(order_schema)
+    chain = translator_llm_prompt | structured_llm
     response = chain.invoke({
         'sentence': data
     })
 
-    return response
+    output=response.model_dump()
 
-re=english_translator(data)
-print(re)
+    return output
