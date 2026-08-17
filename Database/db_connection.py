037cc80c335329fec8f70fdc768e6e0572739b77
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-17 16:05:02 +0500

    Databse work is almost done

---
+++ Database/db_connection.py
@@ -0,0 +1,21 @@
+import os
+import sqlite3
+
+
+
+# Database connection
+
+def db_connection(db_name=None):
+
+    conn=sqlite3.connect(db_name)
+
+    # cursor object to perform actions
+    cursor=conn.cursor()
+
+    # enforcing db to follow foriegn key constraints
+    conn.execute("PRAGMA foreign_keys = ON")
+
+    return cursor,conn
+
+
+
