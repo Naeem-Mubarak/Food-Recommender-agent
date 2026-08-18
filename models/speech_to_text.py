2e2cdecb77bdc76946e4505c3806c3eb220df9eb
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-18 13:43:34 +0500

    Models are finalized

---
+++ models/speech_to_text.py
@@ -0,0 +1,20 @@
+from config.system_info import STT_model
+from config.prompts import voice_llm_prompt
+import io
+
+
+
+def voice_transcript_generator(audio_bytes):
+    audio_file = io.BytesIO(audio_bytes)
+    audio_file.name = "audio.wav"  # Groq's API needs a filename hint, even in-memory
+
+    data = STT_model.audio.transcriptions.create(
+        file=audio_file,
+        model="whisper-large-v3",
+        prompt=voice_llm_prompt,
+        language='en',
+        temperature=0,
+    )
+    return data.text
+
+
