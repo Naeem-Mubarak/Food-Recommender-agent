68c88bce5e5a097421d49b781591ab03f96dc879
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-18 16:15:20 +0500

    Main archeitecture

---
+++ main/main.py
@@ -0,0 +1,31 @@
+from langgraph.graph import START,END,StateGraph
+from Nodes.state import state_schema
+from Nodes.voice_receiver_node import voice_receiver
+from Nodes.Check_user_node import check_user
+from Nodes.history_loader_node import history_loader
+from Nodes.Check_completeness_node import check_completeness_node
+from Nodes.recommend_dishes_node import recommendation
+from Nodes.select_item_node import select_dish
+
+
+graph=StateGraph(state_schema)
+
+graph.add_node('data_receiver',voice_receiver)
+graph.add_node('check_user',check_user)
+graph.add_node('history_loader',history_loader)
+graph.add_node('check_order_completness',check_completeness_node)
+graph.add_node('recommendations',recommendation)
+graph.add_node('select_item',select_dish)
+
+
+graph.add_edge(START,'data_receiver')
+graph.add_edge('data_receiver','check_user')
+graph.add_edge('check_user','history_loader')
+graph.add_edge('history_loader','check_order_completness')
+graph.add_edge('check_order_completness','recommendations')
+graph.add_edge('recommendations','select_item')
+graph.add_edge('select_item',END)
+
+
+
+agent=graph.compile()
