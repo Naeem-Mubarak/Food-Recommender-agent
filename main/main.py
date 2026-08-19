f8ccc79d1fafbf77a7a407edcaf7136db837aa91
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-19 21:37:15 +0500

    architecture update

--- main/main.py
+++ main/main.py
@@ -10,6 +10,8 @@ from Nodes.conditional_node import conditional_node
 from Nodes.new_user_node import new_user
 from Nodes.order_info_completion_node import order_info_completion
 from Nodes.complete_info_node import complete_info
+from Nodes.order_confirmation import confirmation_node
+from Nodes.update_db_node import update_db
 
 graph=StateGraph(state_schema)
 
@@ -21,6 +23,7 @@ graph.add_node('recommendations',recommendation)
 graph.add_node('select_item',select_dish)
 graph.add_node('new_user',new_user)
 graph.add_node('complete_info',complete_info)
+graph.add_node('update_db',update_db)
 
 
 graph.add_edge(START,'data_receiver')
@@ -29,11 +32,12 @@ graph.add_conditional_edges('check_user',conditional_node)
 graph.add_edge('new_user','check_user')
 graph.add_edge('check_user','history_loader')
 graph.add_edge('history_loader','check_order_completness')
-graph.add_edge('check_order_completness',order_info_completion)
+graph.add_conditional_edges('check_order_completness',order_info_completion)
 graph.add_edge('complete_info','check_order_completness')
 graph.add_edge('check_order_completness','recommendations')
 graph.add_edge('recommendations','select_item')
-graph.add_edge('select_item',END)
+graph.add_conditional_edges('select_item',confirmation_node)
+graph.add_edge('update_db',END)
 
 
 
