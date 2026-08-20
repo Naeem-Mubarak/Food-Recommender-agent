077ac85cb2017b70cb20daa2392b0eb64aa8b1cc
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-20 12:52:29 +0500

    changes saved in main file

--- main/main.py
+++ main/main.py
@@ -5,7 +5,7 @@ from Nodes.Check_user_node import check_user
 from Nodes.history_loader_node import history_loader
 from Nodes.Check_completeness_node import check_completeness_node
 from Nodes.recommend_dishes_node import recommendation
-from Nodes.select_item_node import select_dish
+from Nodes.select_item_node import select_item
 from Nodes.conditional_node import conditional_node
 from Nodes.new_user_node import new_user
 from Nodes.order_info_completion_node import order_info_completion
@@ -20,7 +20,7 @@ graph.add_node('check_user',check_user)
 graph.add_node('history_loader',history_loader)
 graph.add_node('check_order_completness',check_completeness_node)
 graph.add_node('recommendations',recommendation)
-graph.add_node('select_item',select_dish)
+graph.add_node('select_item',select_item)
 graph.add_node('new_user',new_user)
 graph.add_node('complete_info',complete_info)
 graph.add_node('update_db',update_db)
@@ -30,11 +30,9 @@ graph.add_edge(START,'data_receiver')
 graph.add_edge('data_receiver','check_user')
 graph.add_conditional_edges('check_user',conditional_node)
 graph.add_edge('new_user','check_user')
-graph.add_edge('check_user','history_loader')
 graph.add_edge('history_loader','check_order_completness')
 graph.add_conditional_edges('check_order_completness',order_info_completion)
 graph.add_edge('complete_info','check_order_completness')
-graph.add_edge('check_order_completness','recommendations')
 graph.add_edge('recommendations','select_item')
 graph.add_conditional_edges('select_item',confirmation_node)
 graph.add_edge('update_db',END)
