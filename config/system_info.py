d21ceb442d12aad937b91632ef84956d4612313a
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-21 15:18:19 +0500

    config updation

--- config/system_info.py
+++ config/system_info.py
@@ -11,6 +11,6 @@ model=ChatGoogleGenerativeAI(model='gemini-3.5-flash')
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(PROJECT_ROOT, "Database", "food_agent.db")
 
-STT_model=Groq(api_key=os.getenv('GROQ_API_KEY'))
+groq_client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 
 DB_URL=os.getenv('DATABASE_URL')
