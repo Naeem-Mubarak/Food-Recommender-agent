7a8b0af7f1a112f550b438cae3d30d4480902dc7
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-08-23 18:34:14 +0500

    Updated database info

--- config/system_info.py
+++ config/system_info.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 model=ChatGoogleGenerativeAI(model='gemini-3.5-flash')
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_PATH = os.path.join(PROJECT_ROOT, "Database", "food_agent.db")
+DB_PATH = os.path.join(PROJECT_ROOT, "database", "food_agent.db")
 
 groq_client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 
