9d82625224309fea7fdca354f28bd1f709425d26
Naeem Mubarak <naeemmubarak777@gmail.com>
2026-09-02 17:31:08 +0500

    fixed bug of adding rejected items

--- backend/graph/nodes/add_rejected_items_node.py
+++ backend/graph/nodes/add_rejected_items_node.py
@@ -10,6 +10,6 @@ def add_rejected_items(state: state_schema):
     if state['recommendation_satisfaction'] == 'no':
 
         # accumulating the list of rejected items which user rejected so they don't come again in the recommendation of user again
-        state['rejected_recommendations'] = state.get(['rejected_recommendations'],[]) + [state['recommendations']]
+        state['rejected_recommendations'] = state.get('rejected_recommendations',[]) + [state['recommendations']]
 
     return state
